56786b0641a2d5633da5aa921f73f194019f98a0
mnelshazly <mnelshazly7@gmail.com>
2025-03-07 14:11:08 +0200

    Implementation of DAL

---
+++ Company.G03.DAL/Data/Configurations/DepartmentConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.G03.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Company.G03.DAL.Data.Configurations
+{
+    internal class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10,10);
+        }
+    }
+}
