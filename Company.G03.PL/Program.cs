a3700c2979df91e23aeb95238e1939b24b166207
mnelshazly <mnelshazly@DESKTOP-OR1B2BR>
2025-03-10 21:34:38 +0200

    Implementation of BLL of Employee Service before Generic

--- Company.G03.PL/Program.cs
+++ Company.G03.PL/Program.cs
@@ -14,6 +14,7 @@ namespace Company.G03.PL
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>(); // Allow DI for DepartmentRepository
+            builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>(); // Allow DI for EmployeeRepository
             builder.Services.AddDbContext<CompanyDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
