2d3fcb4e367db9822f2b84f11f98939086fe614b
mnelshazly <mnelshazly@DESKTOP-OR1B2BR>
2025-03-25 16:37:04 +0200

    Implement SignIn functionality

--- Company.G03.PL/Controllers/AccountController.cs
+++ Company.G03.PL/Controllers/AccountController.cs
@@ -14,6 +14,8 @@ namespace Company.G03.PL.Controllers
             _userManager = userManager;
         }
 
+        // P@ssW0rd
+
         #region SignUp
 
         [HttpGet]
@@ -69,6 +71,38 @@ namespace Company.G03.PL.Controllers
         #endregion
 
         #region SignIn
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInDto model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user is not null)
+                {
+                    var flag = await _userManager.CheckPasswordAsync(user, model.Password);
+
+                    if (flag)
+                    {
+                        // Sign In
+
+                        return RedirectToAction(nameof(HomeController.Index), "Home");
+                    }
+                }
+
+                ModelState.AddModelError("", "Invalid Login !!");
+
+            }
+
+            return View(model);
+        }
 
         #endregion
 
