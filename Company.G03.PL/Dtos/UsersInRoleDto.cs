1992f5f410276494801a4c7081b43b46022fa490
mnelshazly <mnelshazly7@gmail.com>
2025-03-28 21:43:39 +0200

    Implement AddOrRemovUser - GET

---
+++ Company.G03.PL/Dtos/UsersInRoleDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Company.G03.PL.Dtos
+{
+    public class UsersInRoleDto
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
